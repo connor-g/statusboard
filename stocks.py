26a2886a8a76b3284f8f01f6a598e8b94028d7d6
Connor Graham <connor_graham@apple.com>
2015-07-31 14:15:11 -0700

    change from stock name to symbol

--- stocks.py
+++ stocks.py
@@ -7,5 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def stocks():
 	tickers = ['AAPL','CRM']
-	csvData = requests.get('http://finance.yahoo.com/d/quotes.csv?s=' + '+'.join(tickers) + '&f=nap2')
-	return '50%,25%,25%,\n'+csvData.text
+	for ticker in tickers
+	csvData = requests.get('http://finance.yahoo.com/d/quotes.csv?s=' + '+'.join(tickers) + '&f=sap2')
+	return '40%,30%,30%,\n'+csvData.text
