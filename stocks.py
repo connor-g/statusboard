dfe4206bf9aa6fcbf7baef7bad945d98347d1e49
Connor Graham <connor_graham@apple.com>
2015-07-31 16:12:51 -0700

    adding logs

--- stocks.py
+++ stocks.py
@@ -3,6 +3,18 @@ import requests
 from flask import Flask
 
 app = Flask(__name__)
+app.debug = True
+
+app.logger.setLevel(logging.DEBUG)
+del app.logger.handlers[:]
+
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setLevel(logging.DEBUG)
+handler.formatter = logging.Formatter(
+    fmt=u"%(asctime)s level=%(levelname)s %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%SZ",
+)
+app.logger.addHandler(handler)
 
 @app.route('/stocks/<ticker>')
 def stocks():
