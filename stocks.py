155d672048aa66a3e138b20eb528bf91f93e0eae
Connor Graham <connor_graham@apple.com>
2015-07-31 14:59:58 -0700

    testing

--- stocks.py
+++ stocks.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 @app.route('/stocks/<ticker>')
 def stocks():
 	#tickers = tickerlist.split('+')
-	retVal = ''
+	#retVal = ''
 	#for ticker in tickers:
 	csvData = requests.get('http://finance.yahoo.com/d/quotes.csv?s=' + ticker + '&f=sap2')
-	retVal = retVal + csvData.text + '\n'
-	return retVal
+	#retVal = retVal + csvData.text + '\n'
+	#return retVal
+	return csvData.text
 	
 @app.route('/')
 def root():
